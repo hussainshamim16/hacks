847c0cd83b6c165fd152155da5dbac77fe80faf8
HUSSAIN SHAMIM <hj422748@gmail.com>
2026-01-15 14:29:17 +0500

    add services slider section

--- javscript/apps.js
+++ javscript/apps.js
@@ -1,1 +1,20 @@
 console.log("ready")
+document.addEventListener("DOMContentLoaded", function () {
+    // Your JavaScript code here
+    let camoServicesClider = new Swiper(".camo-services-slider", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        // initialSlide: 1,
+        // centeredSlides: false,
+        // autoplay: true,
+        loop: true,
+        grabCursor: true,
+        breakpoints: {
+            0: { slidesPerView: 1, spaceBetween: 1 },
+            768: { slidesPerView: 2, spaceBetween: 10 },
+            1024: { slidesPerView: 2.5, spaceBetween: 10 },
+            1300: { slidesPerView: 3.1, spaceBetween: 20 }
+        }
+    });
+});
+
